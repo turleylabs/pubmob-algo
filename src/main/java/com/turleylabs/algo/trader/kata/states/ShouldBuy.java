ef7b06c6d9ffe16d9ccb4de5ae6cbb05077943dc
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-08-07 17:12:11 +0000

    WIP: flushing out the 2day ShouldBuy logic.  NOT GREEN!

--- src/main/java/com/turleylabs/algo/trader/kata/states/ShouldBuy.java
+++ src/main/java/com/turleylabs/algo/trader/kata/states/ShouldBuy.java
@@ -4,13 +4,39 @@ import com.turleylabs.algo.trader.kata.Averages;
 import com.turleylabs.algo.trader.kata.RefactorMeAlgorithm;
 import com.turleylabs.algo.trader.kata.framework.Slice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ShouldBuy {
+    private ArrayList<Double> day = new ArrayList<Double>(0);
+    private Double halfHigh = 500.0;
+    // new functionality test stub.
+    // Goal:  initiate a buy when the VIX is greater than the threshold currently in the algorithm,
+    // as long as it (the VIX) closes two days in a row below half of it's most recent high
+
     boolean shouldBuy(Slice data, String symbol, Averages averages, double lastVixClose) {
 
-        // create a memory
+        // two days memory
+        if (vixRulesStateBuy()) {
+            return true;
+        }
+
         return averages.arePricesRisingNearTerm(data, symbol)
                 && !(averages.isPriceCloseToPeak(data, symbol))
                 && lastVixClose < RefactorMeAlgorithm.ENTRY_THRESHOLD
                 && averages.isPriceNearShortTermAverage(data, symbol);
     }
+
+    private boolean vixRulesStateBuy() {
+        return twoDayAverageBelowHalfHigh();
+    }
+
+    private boolean twoDayAverageBelowHalfHigh() {
+        if ( day.size() < 2) {
+            return false;
+        }
+        halfHigh = day.stream().mapToDouble().max()
+        return  day.get(0) < halfHigh  && day.get(1) < halfHigh;
+    }
+
 }
