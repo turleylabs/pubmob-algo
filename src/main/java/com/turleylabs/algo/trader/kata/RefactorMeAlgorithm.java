b90d707ef5473af53723628f5afe912ab320cb00
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-08-14 15:58:09 +0000

    Revert "Merge branches 'gesellc-test-push-with-mob-tool' and 'master' of https://github.com/sjturley/pubmob-algo into master"
    This reverts commit 085dbc8e8693b6c799b7920a5070c7f42501f2ab, reversing
    changes made to ef7b06c6d9ffe16d9ccb4de5ae6cbb05077943dc.

--- src/main/java/com/turleylabs/algo/trader/kata/RefactorMeAlgorithm.java
+++ src/main/java/com/turleylabs/algo/trader/kata/RefactorMeAlgorithm.java
@@ -1,118 +1,89 @@
 package com.turleylabs.algo.trader.kata;
 
 import com.turleylabs.algo.trader.kata.framework.*;
+import com.turleylabs.algo.trader.kata.states.*;
 
 import java.time.LocalDate;
+import java.util.Map;
 
-public class RefactorMeAlgorithm extends BaseAlgorithm {
+public class RefactorMeAlgorithm extends BaseAlgorithm implements RefactorMeLogger {
+    public static final double USE_ALL_FUNDS = 1.0;
+    public static final String VIX = "VIX";
 
+    public final ProfitState READY_TO_BUY = new ReadyToBuy(this, symbol1 -> setHoldings(symbol1, USE_ALL_FUNDS), logLine1 -> log2(logLine1), this, new ShouldBuy());
+    public final ProfitState BOUGHT_BELOW_50 = new WeHoldPositions(this, this::liquidate, RefactorMeAlgorithm.this.portfolio, this);
+    public final ProfitState BOUGHT_ABOVE_50 = new BoughtAbove50(this, this::liquidate, RefactorMeAlgorithm.this.portfolio, this);
+    public final ProfitState TOOK_PROFITS = new TookProfits(this);
+
+    Averages averages;
+    private ProfitState profitState = READY_TO_BUY;
+
+    public static final double EXIT_THRESHOLD = 22.0;
+    public static final double ENTRY_THRESHOLD = 19.0;
     String symbol = "TQQQ";
-    SimpleMovingAverage movingAverage200;
-    SimpleMovingAverage movingAverage50;
-    SimpleMovingAverage movingAverage21;
-    SimpleMovingAverage movingAverage10;
-    double previousMovingAverage50;
-    double previousMovingAverage21;
-    double previousMovingAverage10;
     double previousPrice;
     LocalDate previous;
-    CBOE lastVix;
-    boolean boughtBelow50;
-    boolean tookProfits;
+    public CBOE lastVix;
 
     public void initialize() {
         this.setStartDate(2010, 3, 23);  //Set Start Date
         this.setEndDate(2020, 03, 06);
 
         this.setCash(100000);             //Set Strategy Cash
-
-        movingAverage200 = this.createAndAddSimpleMovingAverage(symbol, 200);
-        movingAverage50 = this.createAndAddSimpleMovingAverage(symbol, 50);
-        movingAverage21 = this.createAndAddSimpleMovingAverage(symbol, 21);
-        movingAverage10 = this.createAndAddSimpleMovingAverage(symbol, 10);
+        averages = new Averages(createAndAddSimpleMovingAverage(symbol, 200), createAndAddSimpleMovingAverage(symbol, 50), createAndAddSimpleMovingAverage(symbol, 21), createAndAddSimpleMovingAverage(symbol, 10));
     }
 
     protected void onData(Slice data) {
-        if (previous == getDate()) return;
+        if (previous == getDate()) {
+            return;
+        }
 
-        if (!movingAverage200.isReady()) return;
+        if (!averages.movingAverage200.isReady()) {
+            return;
+        }
 
         if (data.get(symbol) == null) {
             this.log(String.format("No data for symbol %s", symbol));
             return;
         }
 
-        if (data.getCBOE("VIX") != null) {
-            lastVix = data.getCBOE("VIX");
+        if (data.getCBOE(VIX) != null) {
+            lastVix = data.getCBOE(VIX);
         }
 
-        double movingAverage10Value = movingAverage10.getValue();
-        double movingAverage21Value = movingAverage21.getValue();
-        double movingAverage50Value = movingAverage50.getValue();
-        double movingAverage200Value = movingAverage200.getValue();
-
-        if (tookProfits) {
-            if (data.get(symbol).getPrice() < movingAverage10Value) {
-                tookProfits = false;
-            }
-        } else {
-            if (isaBuy()) {
-                buyCalculation(data, movingAverage10Value, movingAverage21Value, movingAverage50Value, movingAverage200Value);
-            } else {
-                sellCalculation(data, movingAverage10Value, movingAverage21Value, movingAverage50Value, movingAverage200Value);
-            }
-        }
+        setState(data);
 
         previous = getDate();
-        previousMovingAverage50 = movingAverage50Value;
-        previousMovingAverage21 = movingAverage21Value;
-        previousMovingAverage10 = movingAverage10Value;
+        averages.updatePreviousAverages();
         previousPrice = data.get(symbol).getPrice();
     }
 
-    private boolean isaBuy() {
-        return portfolio.getOrDefault(symbol, Holding.Default).getQuantity() == 0;
+    private void setState(Slice data) {
+        profitState = profitState.onData(data, symbol, averages, lastVix.getClose());
     }
 
-    private void sellCalculation(Slice data, double movingAverage10Value, double movingAverage21Value, double movingAverage50Value, double movingAverage200Value) {
+    @Override
+    public void logSellAction(Slice data, Averages averages, String symbol, CBOE lastVix, Map<String, Holding> portfolio) {
+        double close = lastVix.getClose();
         double change = (data.get(symbol).getPrice() - portfolio.get(symbol).getAveragePrice()) / portfolio.get(symbol).getAveragePrice();
 
-        if (data.get(symbol).getPrice() < (movingAverage50Value * .93) && !boughtBelow50) {
-            this.log(String.format("Sell %s loss of 50 day. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
-            this.liquidate(symbol);
-        } else {
-            if ((double) (lastVix.getClose()) > 22.0) {
-                this.log(String.format("Sell %s high volatility. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
-                this.liquidate(symbol);
-            } else {
-                if (movingAverage10Value < 0.97 * movingAverage21Value) {
-                    this.log(String.format("Sell %s 10 day below 21 day. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
-                    this.liquidate(symbol);
-                } else {
-                    if (data.get(symbol).getPrice() >= (movingAverage50Value * 1.15) && data.get(symbol).getPrice() >= (movingAverage200Value * 1.40)) {
-                        this.log(String.format("Sell %s taking profits. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
-                        this.liquidate(symbol);
-                        tookProfits = true;
-                    }
-                }
-            }
+        if (averages.priceBelow50DayMAByAtLeast(data, symbol, .07)) {
+            log(String.format("Sell %s loss of 50 day. Gain %.4f. Vix %.4f", symbol, change, close));
+        } else if (hasHighVolatility(lastVix)) {
+            log(String.format("Sell %s high volatility. Gain %.4f. Vix %.4f", symbol, change, close));
+        } else if (averages.did10DayMACrossBelow21DayMA()) {
+            log(String.format("Sell %s 10 day below 21 day. Gain %.4f. Vix %.4f", symbol, change, close));
+        } else if (averages.isPriceCloseToPeak(data, symbol)) {
+            log(String.format("Sell %s taking profits. Gain %.4f. Vix %.4f", symbol, change, close));
         }
     }
 
-    private void buyCalculation(Slice data, double movingAverage10Value, double movingAverage21Value, double movingAverage50Value, double movingAverage200Value) {
-        if (data.get(symbol).getPrice() > movingAverage10Value
-                && movingAverage10Value > movingAverage21Value
-                && movingAverage10Value > previousMovingAverage10
-                && movingAverage21Value > previousMovingAverage21
-                && (double) (lastVix.getClose()) < 19.0
-                && !(data.get(symbol).getPrice() >= (movingAverage50Value * 1.15) && data.get(symbol).getPrice() >= (movingAverage200Value * 1.40))
-                && (data.get(symbol).getPrice() - movingAverage10Value) / movingAverage10Value < 0.07) {
-            this.log(String.format("Buy %s Vix %.4f. above 10 MA %.4f", symbol, lastVix.getClose(), (data.get(symbol).getPrice() - movingAverage10Value) / movingAverage10Value));
-            double amount = 1.0;
-            this.setHoldings(symbol, amount);
-
-            boughtBelow50 = data.get(symbol).getPrice() < movingAverage50Value;
-        }
+    public boolean hasHighVolatility(CBOE lastVix) {
+        return lastVix.getClose() > EXIT_THRESHOLD;
     }
 
+    @Override
+    public void log2(String logline) {
+        log(logline);
+    }
 }
