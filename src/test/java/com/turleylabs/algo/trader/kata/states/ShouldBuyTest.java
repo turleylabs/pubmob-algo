9e8aab6b6acce5ea0e3d45fd6291e45ff9a7ae87
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-08-14 16:40:53 +0000

    t cleanup tests
    Co-Authored-By: Steve Turley <steve@turleylabs.com>
    Co-Authored-By: cybereagle98 <24728632+cybereagle98@users.noreply.github.com>
    Co-Authored-By: Dave M <1200017+dsm2005@users.noreply.github.com>
    Co-Authored-By: Rahul Agrawal <rahulagrawal@gmail.com>
    Co-Authored-By: Paige Watson <paige.watson@outlook.com>
    Co-Authored-By: Luis F. Gutierrez  <8312698+gutierlf@users.noreply.github.com>
    Co-Authored-By: Jeff Langr <jeff@langrsoft.com>

--- src/test/java/com/turleylabs/algo/trader/kata/states/ShouldBuyTest.java
+++ src/test/java/com/turleylabs/algo/trader/kata/states/ShouldBuyTest.java
@@ -9,30 +9,32 @@ public class ShouldBuyTest {
     // Goal:  initiate a buy when the VIX is greater than the threshold currently in the algorithm,
     // as long as it (the VIX) closes two days in a row below half of it's most recent high
     @Test
-    public void somethingShouldBuy(){
-        var shouldBuy = new ShouldBuy();
-        var data = ReadyToBuyTest.getSlice(100);
-        String symbol = "TQQQ";
-        Averages averages = ReadyToBuyTest.createAverages(0,0,0,0);
+    public void shouldBuyHiLoLoLo(){
+        double day1 = 100;
+        double day2 = 42;
+        double day3 = 40;
+        double day4 = 40;
+        boolean finalDayBought = true;
 
-        shouldBuy.shouldBuy(data, symbol,  averages, (double) 100);
-        shouldBuy.shouldBuy(data, symbol, averages, (double) 42);
-        shouldBuy.shouldBuy(data, symbol, averages, (double) 40);
-        boolean result = shouldBuy.shouldBuy(data, symbol, averages, (double) 40);
-        Assert.assertEquals(true, result);
+        verifyShouldBuy(finalDayBought, day1, day2, day3, day4);
     }
+
     @Test
-    public void nothingShouldBuy(){
+    public void loHiLoLo(){
+        verifyShouldBuy(false, (double) 50, (double) 42, (double) 40, (double) 40);
+    }
+
+    private void verifyShouldBuy(boolean finalDayBought, double... days) {
         var shouldBuy = new ShouldBuy();
         var data = ReadyToBuyTest.getSlice(100);
         String symbol = "TQQQ";
-        Averages averages = ReadyToBuyTest.createAverages(0,0,0,0);
+        Averages averages = ReadyToBuyTest.createAverages(0, 0, 0, 0);
 
-        shouldBuy.shouldBuy(data, symbol,  averages, (double) 50);
-        shouldBuy.shouldBuy(data, symbol, averages, (double) 42);
-        shouldBuy.shouldBuy(data, symbol, averages, (double) 40);
-        boolean result = shouldBuy.shouldBuy(data, symbol, averages, (double) 40);
-        Assert.assertEquals(false, result);
+        shouldBuy.shouldBuy(data, symbol, averages, days[0]);
+        shouldBuy.shouldBuy(data, symbol, averages, days[1]);
+        shouldBuy.shouldBuy(data, symbol, averages, days[2]);
+        boolean result = shouldBuy.shouldBuy(data, symbol, averages, days[3]);
+        Assert.assertEquals(finalDayBought, result);
     }
 
 
